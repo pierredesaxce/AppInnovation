b21346ac01f22c15f97bcbd12eb57cbcbdc816b0
anthony-vitrant <anthony.vitrant@alumni.univ-avignon.fr>
2023-11-09 12:43:26 +0100

    u

--- PasswordGeneratorModelAI.py
+++ PasswordGeneratorModelAI.py
@@ -3,23 +3,28 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import train_test_split
 
 # Check available GPUs
 gpus = tf.config.experimental.list_physical_devices('GPU')
 
 if gpus:
     try:
-        # Set the GPU device as the default device
+        # Limit GPU memory usage
         tf.config.experimental.set_memory_growth(gpus[0], True)
         tf.config.set_visible_devices(gpus[0], 'GPU')
+
+        # Limit GPU memory fraction
+        tf.config.experimental.set_virtual_device_configuration(
+            gpus[0],
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)]  # Adjust the limit as needed
+        )
+
         print("Using GPU:", gpus[0])
     except RuntimeError as e:
         print(e)
 else:
     print("No GPUs available. Using CPU.")
-
-# Now you can build and train your model as usual
-# ...
 
 # Chargement du fichier de mots de passe (un mot de passe par ligne)
 with open("data/Ashley-Madison.txt", "r") as file:
@@ -41,15 +46,25 @@ for i, password in enumerate(passwords):
         if t < max_len - 1:
             y[i, t + 1, char_indices[char]] = 1
 
+# Diviser les données en ensembles d'entraînement et de test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 # Création du modèle RNN avec une couche GRU en utilisant Keras
 model = keras.Sequential()
 model.add(layers.GRU(128, input_shape=(max_len, len(chars)), return_sequences=True))
 model.add(layers.TimeDistributed(layers.Dense(len(chars), activation="softmax")))
 
-model.compile(loss="categorical_crossentropy", optimizer="adam")
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])  # Ajout de 'accuracy' comme métrique
 
 # Define a ModelCheckpoint callback
-checkpoint = ModelCheckpoint("best_model.h5", monitor='val_categorical_accuracy', save_best_only=True, mode='max', verbose=1)
+checkpoint = ModelCheckpoint("best_model.h5", monitor='val_accuracy', save_best_only=True, mode='max', verbose=1)  # Changement du monitor
 
-# Entraînement du modèle
-model.fit(X, y, batch_size=128, epochs=1, callbacks=[checkpoint])
+# Entraînement du modèle sur l'ensemble d'entraînement
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=64, epochs=1, callbacks=[checkpoint])
+
+# Afficher l'accuracy sur les ensembles d'entraînement et de test
+train_accuracy = history.history['accuracy']
+test_accuracy = history.history['val_accuracy']
+
+print(f"Accuracy on training set: {train_accuracy[-1]}")
+print(f"Accuracy on test set: {test_accuracy[-1]}")
