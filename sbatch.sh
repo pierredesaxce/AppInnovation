ae1da9e25eb7e370bd062ed9e42acff8d0f4cf16
anthony-vitrant <anthony.vitrant@alumni.univ-avignon.fr>
2023-11-15 12:30:04 +0100

    nan nan jvous ldis

---
+++ sbatch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=TP_INNOV
+#SBATCH --ntsaks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --hint=nomultithread
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
+#SBATCH --gpus-per-node=1
+#SBATCH --partition=gpu
+#SBATCH --time=00:03:00
+#SBATCH --outpout=./logs/
+#SBATCH --error=./logs/
+
+srun ./Pytorch_PasswordGeneratorModelAI_V3.py
