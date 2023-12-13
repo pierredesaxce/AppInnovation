57e622ffc5ae3a313ba897c53db5f0f973841987
Anthony Vitrant <anthony.vitrant@alumni.univ-avignon.fr>
2023-12-13 11:02:30 +0100

    sbatch 128GB

--- sbatch.sh
+++ sbatch.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --hint=nomultithread
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=64G
+#SBATCH --mem=128G
 #SBATCH --gpus-per-node=3
 #SBATCH --partition=gpu
 #SBATCH --time=00:59:00
